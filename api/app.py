d9c1768216e5336a88ed3d9e9a9596e473712afb
learningAItogether <sheldonyue@sina.com>
2022-05-29 10:04:17 +0800

    sixth commit

--- api/app.py
+++ api/app.py
@@ -6,8 +6,16 @@ from flask import request
 #6 可以使用前端模板文件 html文件
 from flask import render_template
 
+#9 引入消息闪现
+from flask import flash
+
+#10 引入重定向
+from flask import redirect
+
 #1构建Flask实例 创建服务对象
 app = Flask(__name__)
+#使用session需要配置密钥
+app.secret_key = "2020-03-24"
 
 @app.route("/",methods = ["GET"])
 def userinfo():
@@ -19,7 +27,10 @@ def result():
     if request.method == "POST":
         username = request.form["username"]
         if  username == "":
-            return "请填写表单！"
+            #8 使用消息闪现将错误信息传入下次请求
+            flash("用户名必须填写!")
+            # 9 再次回到填写信息页面
+            return redirect("/")
         else:
             return render_template("result.html",username = username, safe = False)
 
